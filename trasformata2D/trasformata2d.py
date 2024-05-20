1b1474ee98cc6b2c1c8b1bbc49d1e2f2293b6f20
LeoNiik <leonardonicoletta32@gmail.com>
2024-05-20 11:23:02 +0200

    prima parte esercitazioen4

--- trasformata2D/trasformata2d.py
+++ trasformata2D/trasformata2d.py
@@ -6,36 +6,85 @@ from scipy import signal,fft
 import numpy as np
 from PIL import Image
 
+import cv2
 
-path_to_img = 'line_30deg.png'
+class Filter2D():
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.mask = self.getMask()
+
+    def getMask(self):
+        radius = 50  # Adjust the radius as needed
+        mask = np.zeros((self.width, self.height))
+        center_x, center_y = self.width // 2, self.height // 2
+        for x in range(self.width):
+            for y in range(self.height):
+                if (x - center_x) ** 2 + (y - center_y) ** 2 <= radius ** 2:
+                    mask[x, y] = 1
+        return mask
+        
+
+
+path_to_img = 'saltandpepper_lena.jpg'
 
 img = Image.open(path_to_img)
-img.convert('RGB')
+
 width, height = img.size
 
 
 
 def getRGBvalues(image, mode='rgb'):
+    image = image.convert('RGB')
     rgbvalues = []
     for x in range(0, width):
         for y in range(0, height):
-                rgbvalues.append(image.getpixel((x,y)))           
+                rgbvalues.append(image.getpixel((x,y)))  
+                print(image.getpixel((x,y)))         
     if 'r' == mode:
-        return rgbvalues[:,0]
+        return [i[0] for i in rgbvalues]
     elif 'g' == mode:
-        return rgbvalues[:,1]
+        return [i[1] for i in rgbvalues]
     elif 'b' == mode:
-        return rgbvalues[:,2]
+        return [i[2] for i in rgbvalues]
     else:
         return rgbvalues
     
 
 #scarto un canale 
-red_channel = getRGBvalues(img)[:,0]
-# freq = fft.rfftfreq(len(red_channel),1./fs)
+red_channel = getRGBvalues(img, 'r')
 print(red_channel)
 
-# fft_red = fft.rftt(red_channel)
+red_channel = np.array(red_channel).reshape(width,height) 
+
+# # Display the magnitude of the Fourier Transform
+# cv2.imshow('Fourier Transform', red_channel)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#calcolo la fft, shifto le 
+fourier = fft.fft2(red_channel)
+shift = fft.fftshift(fourier)
+print(shift)
+no_shift_magnitude = np.log(abs(fourier)+1) 
+magnitude = np.log(abs(shift)+1)
+
+filter = Filter2D(width, height)
+filtered = shift*filter.mask
+filtered_magnitude = np.log(abs(filtered)+1)
+
+plt.figure("Not shifted")
+plt.imshow(no_shift_magnitude)
+plt.title("Not shifted")
+
+plt.figure("Filtered")
+plt.title("Filtered")
+plt.imshow(magnitude)
+
+plt.figure("Shifted")
+plt.title("Shifted")
+plt.imshow(filtered_magnitude)
+plt.show()
 
 
 
