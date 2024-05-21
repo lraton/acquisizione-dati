50ab33037afd2e0fb732bf3eb649bfca3713a04e
LeoNiik <leonardonicoletta32@gmail.com>
2024-05-21 11:33:03 +0200

    trasformata2d

--- trasformata2D/trasformata2d.py
+++ trasformata2D/trasformata2d.py
@@ -2,20 +2,22 @@
 
 
 import matplotlib.pyplot as plt
-from scipy import signal,fft
+from scipy import signal,fft, ndimage
 import numpy as np
 from PIL import Image
 
 import cv2
 
-class Filter2D():
-    def __init__(self, width, height) -> None:
+
+class CircleFilter2D():
+    def __init__(self, radius, width, height) -> None:
+        self.radius = radius
         self.width = width
         self.height = height
         self.mask = self.getMask()
 
     def getMask(self):
-        radius = 50  # Adjust the radius as needed
+        radius = self.radius  # Adjust the radius as needed
         mask = np.zeros((self.width, self.height))
         center_x, center_y = self.width // 2, self.height // 2
         for x in range(self.width):
@@ -23,39 +25,16 @@ class Filter2D():
                 if (x - center_x) ** 2 + (y - center_y) ** 2 <= radius ** 2:
                     mask[x, y] = 1
         return mask
-        
 
 
 path_to_img = 'saltandpepper_lena.jpg'
 
 img = Image.open(path_to_img)
-
+img= img.convert('RGB')
 width, height = img.size
 
-
-
-def getRGBvalues(image, mode='rgb'):
-    image = image.convert('RGB')
-    rgbvalues = []
-    for x in range(0, width):
-        for y in range(0, height):
-                rgbvalues.append(image.getpixel((x,y)))  
-                print(image.getpixel((x,y)))         
-    if 'r' == mode:
-        return [i[0] for i in rgbvalues]
-    elif 'g' == mode:
-        return [i[1] for i in rgbvalues]
-    elif 'b' == mode:
-        return [i[2] for i in rgbvalues]
-    else:
-        return rgbvalues
-    
-
 #scarto un canale 
-red_channel = getRGBvalues(img, 'r')
-print(red_channel)
-
-red_channel = np.array(red_channel).reshape(width,height) 
+red_channel, green_channel, blue_channel= img.split()
 
 # # Display the magnitude of the Fourier Transform
 # cv2.imshow('Fourier Transform', red_channel)
@@ -65,18 +44,38 @@ red_channel = np.array(red_channel).reshape(width,height)
 #calcolo la fft, shifto le 
 fourier = fft.fft2(red_channel)
 shift = fft.fftshift(fourier)
-print(shift)
 no_shift_magnitude = np.log(abs(fourier)+1) 
 magnitude = np.log(abs(shift)+1)
 
-filter = Filter2D(width, height)
+filter = CircleFilter2D(50,width, height)
 filtered = shift*filter.mask
 filtered_magnitude = np.log(abs(filtered)+1)
+
+gaussian_filtered = ndimage.gaussian_filter(red_channel,2) 
+
+median_filtered = ndimage.median_filter(red_channel, size= 20) 
+
+unshift = fft.ifftshift(filtered)
+filtered_img = fft.ifft2(unshift)
+filtered_img = np.abs(filtered_img)
+
+norm_data = (filtered_img - np.min(filtered_img)) / (np.max(filtered_img) -np.min(filtered_img))
+scaled_data = (norm_data*255).astype(np.uint8)
+scaled_arr = scaled_data
+
+# new_img = Image.new('RGB', (width,height))
+color = [[(i, 0, 0)] for i in scaled_data]
+prova = np.full((height,width), 300)
+print(prova)
+
+image_array = np.array(prova)
+new_img = Image.fromarray(image_array)
+new_img.show()
+
 
 plt.figure("Not shifted")
 plt.imshow(no_shift_magnitude)
 plt.title("Not shifted")
-
 plt.figure("Filtered")
 plt.title("Filtered")
 plt.imshow(magnitude)
@@ -84,6 +83,15 @@ plt.imshow(magnitude)
 plt.figure("Shifted")
 plt.title("Shifted")
 plt.imshow(filtered_magnitude)
+
+plt.figure("Gaussian Filtered")
+plt.title("Gaussian Filtered")
+plt.imshow(gaussian_filtered)
+
+plt.figure("Median Filtered")
+plt.title("Median Filtered")
+plt.imshow(median_filtered)
+
 plt.show()
 
 
