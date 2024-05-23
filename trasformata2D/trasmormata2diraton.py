d71d68c7cadbc4fc852675df1c711bcc0c349072
lraton <notari.filippo@outlook.it>
2024-05-23 09:48:30 +0200

    fix trassformata 2d

--- trasformata2D/trasmormata2diraton.py
+++ trasformata2D/trasmormata2diraton.py
@@ -9,6 +9,7 @@ from scipy.ndimage import median_filter
 import numpy as np
 from PIL import Image
 
+##### Eserczio 1
 imgs = ["line_0deg.png", "line_30deg.png", "line_60deg.png", "line_90deg.png", "line_0deg.png"]
 
 # Itera attraverso ogni immagine nell'elenco
@@ -29,19 +30,55 @@ for immagine in imgs:
     fft_result_shifted = scipy.fft.fftshift(fft2)
 
     # Compute the magnitude spectrum
-    spectrum = np.log(abs(fft_result_shifted)+1)
+    magnitude_spectrum = np.log(abs(fft_result_shifted)+1)
 
     # Plot the magnitude spectrum
     plt.figure(figsize=(6, 6))
-    plt.imshow(spectrum)
+    plt.imshow(magnitude_spectrum)
     plt.title('Spectrum '+path_to_img)
     plt.colorbar()
 
 
+##### Eserczio 2
+path_to_img = 'lena_std.tif'
+img = Image.open(path_to_img)
+img = img.convert('RGB')
+plt.figure(figsize=(6, 6))
+plt.title(path_to_img)
+plt.imshow(img)
+
+plt.colorbar()
+width, height = img.size
+
+#scarto un canale 
+red_channel, green_channel, blue_channel = img.split()
+
+fft2 = scipy.fft.fft2(red_channel, s=None, axes=(- 2, - 1), norm=None, 
+               overwrite_x=False, workers=None, plan=None)
+
+fft_result_shifted = scipy.fft.fftshift(fft2)
+
+# Compute the magnitude spectrum
+magnitude_spectrum = np.log(abs(fft_result_shifted)+1)
+
+radius = 50  # Adjust the radius as needed
+mask = np.zeros((width, height))
+center_x, center_y = width // 2, height // 2
+for x in range(width):
+    for y in range(height):
+        if (x - center_x) ** 2 + (y - center_y) ** 2 <= radius ** 2:
+            mask[x, y] = 1
+
+spectrum_mask = magnitude_spectrum * mask
+
+# Plot the spectrum with mask
+plt.figure(figsize=(6, 6))
+plt.imshow(spectrum_mask)
+plt.title('Spectrum with mask'+path_to_img)
+plt.colorbar()
 
 
-
-#####
+##### Esercizio 3
 path_to_img = 'saltandpepper_lena.jpg'
 img = Image.open(path_to_img)
 img = img.convert('RGB')
@@ -61,7 +98,7 @@ fft2 = scipy.fft.fft2(red_channel, s=None, axes=(- 2, - 1), norm=None,
 fft_result_shifted = scipy.fft.fftshift(fft2)
 
 # Compute the magnitude spectrum
-spectrum = np.log(abs(fft_result_shifted)+1)
+magnitude_spectrum = np.log(abs(fft_result_shifted)+1)
 
 radius = 50  # Adjust the radius as needed
 mask = np.zeros((width, height))
@@ -71,14 +108,13 @@ for x in range(width):
         if (x - center_x) ** 2 + (y - center_y) ** 2 <= radius ** 2:
             mask[x, y] = 1
 
-magnitude_mask = spectrum * mask
+spectrum_mask = magnitude_spectrum * mask
 
 # Plot the spectrum with mask
 plt.figure(figsize=(6, 6))
-plt.imshow(magnitude_mask)
+plt.imshow(spectrum_mask)
 plt.title('Spectrum with mask'+path_to_img)
 plt.colorbar()
-
 
 # Plot the Gaussian Filter
 gaussian=gaussian_filter(red_channel, sigma=1)
